341583f1aeab7f64363677e0447cabf62db59401
Marlon Matthew <mbm.drums@gmail.com>
2021-01-20 19:38:02 -0800

    added color boxes for UV index. User is informed that times are in GMT. The current condition icon and description are now in the current div.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Description
 
-![Capture](https://user-images.githubusercontent.com/73320305/105269826-8bbb9a00-5b49-11eb-9708-e976db7e4d24.PNG)
+![Capture](https://user-images.githubusercontent.com/73320305/105276659-bd872d80-5b56-11eb-818e-9df42a604154.PNG)
 
 User searches for a city anywhere in the world and will be able to see current and 5 day weather forecast conditions.
 The cities searched are stored in local storage and the user can click on the city buttons that are generated to view the weather data.
--- script.js
+++ script.js
@@ -33,6 +33,34 @@ $(document).ready(function () {
                 var uvIndex = response.current.uvi;
                 $("#uvIndex").append(`UV Index: ${uvIndex}`);
 
+                // Display UV Index Color
+                if (uvIndex < 3) {
+                    var greenURL = `uvi-low.jpg`;
+                    var green = $(`<img>`).attr(`src`, greenURL);
+                    green.addClass("color")
+                    $("#uvIndex").append(green)
+                } else if (uvIndex < 6) {
+                    var yellowURL = `uvi-moderate.jpg`;
+                    var yellow = $(`<img>`).attr(`src`, yellowURL);
+                    yellow.addClass("color")
+                    $("#uvIndex").append(yellow)
+                } else if (uvIndex < 8) {
+                    var orangeURL = `uvi-high.jpg`;
+                    var orange = $(`<img>`).attr(`src`, orangeURL);
+                    orange.addClass("color")
+                    $("#uvIndex").append(orange)
+                } else if (uvIndex < 11) {
+                    var redURL = `uvi-veryhigh.jpg`;
+                    var red = $(`<img>`).attr(`src`, redURL);
+                    red.addClass("color")
+                    $("#uvIndex").append(red)
+                } else {
+                    var purpleURL = `uvi-extreme.jpg`;
+                    var purple = $(`<img>`).attr(`src`, purpleURL);
+                    purple.addClass("color")
+                    $("#uvIndex").append(purple)
+                };
+
             });
 
     };
@@ -56,7 +84,15 @@ $(document).ready(function () {
 
                 // Display the city
                 var currentCity = response.name;
-                $("#city").append(`City: ${currentCity}`);
+                $("#city").append(currentCity);
+
+                // Display the condition
+                var icon = response.weather[0].icon;
+                var iconURL = `http://openweathermap.org/img/w/${icon}.png`;
+                var icon_ = $('<img>').attr('src', iconURL);
+                $("#condition").append(icon_);
+                var condition = response.weather[0].main;
+                $("#condition").append(` ${condition}`);
 
                 // Display the temp
                 var temp = response.main.temp;
@@ -64,7 +100,7 @@ $(document).ready(function () {
 
                 // Display the humidity
                 var humidity = response.main.humidity;
-                $("#humidity").append(`Humidity: ${humidity}%`);
+                $("#humidity").append(`Humidity: ${humidity}% `);
 
                 // Display the wind speed
                 var windSpeed = response.wind.speed;
@@ -97,74 +133,74 @@ $(document).ready(function () {
                 console.log(response);
 
                 // Display the date
-                var date1 = response.list[3].dt_txt;
-                var date2 = response.list[11].dt_txt;
-                var date3 = response.list[19].dt_txt;
-                var date4 = response.list[27].dt_txt;
-                var date5 = response.list[35].dt_txt;
-                $("#date1").append(`Date: ${date1}`);
-                $("#date2").append(`Date: ${date2}`);
-                $("#date3").append(`Date: ${date3}`);
-                $("#date4").append(`Date: ${date4}`);
-                $("#date5").append(`Date: ${date5}`);
+                var date1 = response.list[4].dt_txt;
+                var date2 = response.list[12].dt_txt;
+                var date3 = response.list[20].dt_txt;
+                var date4 = response.list[28].dt_txt;
+                var date5 = response.list[36].dt_txt;
+                $("#date1").append(date1);
+                $("#date2").append(date2);
+                $("#date3").append(date3);
+                $("#date4").append(date4);
+                $("#date5").append(date5);
 
                 //Display the weather
-                var weather1 = response.list[3].weather[0].main;
-                var weather2 = response.list[11].weather[0].main;
-                var weather3 = response.list[19].weather[0].main;
-                var weather4 = response.list[27].weather[0].main;
-                var weather5 = response.list[35].weather[0].main;
-                $("#weather1").append(`Weather: ${weather1}`);
-                $("#weather2").append(`Weather: ${weather2}`);
-                $("#weather3").append(`Weather: ${weather3}`);
-                $("#weather4").append(`Weather: ${weather4}`);
-                $("#weather5").append(`Weather: ${weather5}`);
+                var weather1 = response.list[4].weather[0].main;
+                var weather2 = response.list[12].weather[0].main;
+                var weather3 = response.list[20].weather[0].main;
+                var weather4 = response.list[28].weather[0].main;
+                var weather5 = response.list[36].weather[0].main;
+                $("#weather1").append(weather1);
+                $("#weather2").append(weather2);
+                $("#weather3").append(weather3);
+                $("#weather4").append(weather4);
+                $("#weather5").append(weather5);
 
                 // Display the icon
-                var icon1 = response.list[3].weather[0].icon;
-                var iconURL = `http://openweathermap.org/img/w/${icon1}.png`;
-                var icon_1 = $('<img>').attr('src', iconURL);
+                var icon1 = response.list[4].weather[0].icon;
+                var icon1URL = `http://openweathermap.org/img/w/${icon1}.png`;
+                var icon_1 = $('<img>').attr('src', icon1URL);
                 $("#icon1").append(icon_1);
-                var icon2 = response.list[11].weather[0].icon;
-                var iconURL = `http://openweathermap.org/img/w/${icon2}.png`;
-                var icon_2 = $('<img>').attr('src', iconURL);
+                var icon2 = response.list[12].weather[0].icon;
+                var icon2URL = `http://openweathermap.org/img/w/${icon2}.png`;
+                var icon_2 = $('<img>').attr('src', icon2URL);
                 $("#icon2").append(icon_2);
-                var icon3 = response.list[19].weather[0].icon;
-                var iconURL = `http://openweathermap.org/img/w/${icon3}.png`;
-                var icon_3 = $('<img>').attr('src', iconURL);
+                var icon3 = response.list[20].weather[0].icon;
+                var icon3URL = `http://openweathermap.org/img/w/${icon3}.png`;
+                var icon_3 = $('<img>').attr('src', icon3URL);
                 $("#icon3").append(icon_3);
-                var icon4 = response.list[27].weather[0].icon;
-                var iconURL = `http://openweathermap.org/img/w/${icon4}.png`;
-                var icon_4 = $('<img>').attr('src', iconURL);
+                var icon4 = response.list[28].weather[0].icon;
+                var icon4URL = `http://openweathermap.org/img/w/${icon4}.png`;
+                var icon_4 = $('<img>').attr('src', icon4URL);
                 $("#icon4").append(icon_4);
-                var icon5 = response.list[35].weather[0].icon;
-                var iconURL = `http://openweathermap.org/img/w/${icon5}.png`;
-                var icon_5 = $('<img>').attr('src', iconURL);
+                var icon5 = response.list[36].weather[0].icon;
+                var icon5URL = `http://openweathermap.org/img/w/${icon5}.png`;
+                var icon_5 = $('<img>').attr('src', icon5URL);
                 $("#icon5").append(icon_5);
 
                 // Display the temp
-                var temp1 = response.list[3].main.temp;
-                var temp2 = response.list[11].main.temp;
-                var temp3 = response.list[19].main.temp;
-                var temp4 = response.list[27].main.temp;
-                var temp5 = response.list[35].main.temp;
-                $("#temp1").append(`Temperature: ${temp1}°F`);
-                $("#temp2").append(`Temperature: ${temp2}°F`);
-                $("#temp3").append(`Temperature: ${temp3}°F`);
-                $("#temp4").append(`Temperature: ${temp4}°F`);
-                $("#temp5").append(`Temperature: ${temp5}°F`);
+                var temp1 = response.list[4].main.temp;
+                var temp2 = response.list[12].main.temp;
+                var temp3 = response.list[20].main.temp;
+                var temp4 = response.list[28].main.temp;
+                var temp5 = response.list[36].main.temp;
+                $("#temp1").append(`Temp: ${temp1} °F`);
+                $("#temp2").append(`Temp: ${temp2} °F`);
+                $("#temp3").append(`Temp: ${temp3} °F`);
+                $("#temp4").append(`Temp: ${temp4} °F`);
+                $("#temp5").append(`Temp: ${temp5} °F`);
 
                 // Display the humidity
-                var humidity1 = response.list[3].main.humidity;
-                var humidity2 = response.list[11].main.humidity;
-                var humidity3 = response.list[19].main.humidity;
-                var humidity4 = response.list[27].main.humidity;
-                var humidity5 = response.list[35].main.humidity;
-                $("#humidity1").append(`Humidity: ${humidity1}`);
-                $("#humidity2").append(`Humidity: ${humidity2}`);
-                $("#humidity3").append(`Humidity: ${humidity3}`);
-                $("#humidity4").append(`Humidity: ${humidity4}`);
-                $("#humidity5").append(`Humidity: ${humidity5}`);
+                var humidity1 = response.list[4].main.humidity;
+                var humidity2 = response.list[12].main.humidity;
+                var humidity3 = response.list[20].main.humidity;
+                var humidity4 = response.list[28].main.humidity;
+                var humidity5 = response.list[36].main.humidity;
+                $("#humidity1").append(`Humidity: ${humidity1}%`);
+                $("#humidity2").append(`Humidity: ${humidity2}%`);
+                $("#humidity3").append(`Humidity: ${humidity3}%`);
+                $("#humidity4").append(`Humidity: ${humidity4}%`);
+                $("#humidity5").append(`Humidity: ${humidity5}%`);
 
             });
 
@@ -181,6 +217,7 @@ $(document).ready(function () {
 
         // Clear all data
         $("#city").empty();
+        $("#condition").empty();
         $("#temp").empty();
         $("#humidity").empty();
         $("#windSpeed").empty();
